2e7928df988ce4857c85d78174ac7f352ccff5c6
Maxiloud <maxime@les-tilleuls.coop>
2026-01-07 11:51:44 +0100

    wip

--- wp-content/themes/tilleuls-cabinet-ortego-theme/functions.php
+++ wp-content/themes/tilleuls-cabinet-ortego-theme/functions.php
@@ -17,11 +17,31 @@
  * @return void
  */
 
+function tilleuls_cabinet_ortego_theme_register_blocks(): void
+{
+    // On définit le chemin vers le DOSSIER qui contient block.json
+    $block_path = __DIR__ . '/includes/blocks/hero-homepage';
+
+    // On tente d'enregistrer
+    $result = register_block_type($block_path);
+
+    // DÉBOGAGE : Si cela échoue, on l'écrit dans le debug.log ou on affiche une erreur admin
+    if ( ! $result ) {
+        error_log( 'ERREUR : Impossible d\'enregistrer le bloc au chemin : ' . $block_path );
+        // Si le fichier block.json est introuvable ou invalide, register_block_type renvoie false
+    }
+}
+add_action('init', 'tilleuls_cabinet_ortego_theme_register_blocks');
+
+/**
+ *Custom Post Type
+ */
 
 require_once realpath(__DIR__ . '/includes/post-types/expertise.php');
 require_once realpath(__DIR__ . '/includes/post-types/union.php');
 
-function tilleuls_cabinet_ortego_theme_styles() {
+function tilleuls_cabinet_ortego_theme_styles(): void
+{
 	wp_enqueue_style(
 		'tilleuls-cabinet-ortego-theme-style',
 		get_stylesheet_uri(),
--- wp-content/themes/tilleuls-cabinet-ortego-theme/patterns/header.php
+++ wp-content/themes/tilleuls-cabinet-ortego-theme/patterns/header.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Title: Header
  * Slug: tilleuls-cabinet-ortego-theme/header
@@ -17,6 +15,19 @@ declare(strict_types=1);
 ?>
 <!-- wp:group {"align":"full","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull">
-    <h1>HELLLO MOTO</h1>
+    <!-- wp:group {"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+        <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+        <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
+            <!-- wp:site-title {"level":0} /-->
+            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+            <div class="wp-block-group">
+                <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"right"}} /-->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
