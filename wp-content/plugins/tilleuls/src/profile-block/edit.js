7b2d509955904d0fefbeec6ebc92bdaeef8c9b54
maxiloud <61539540+maxime-urbanski@users.noreply.github.com>
2026-01-14 16:51:32 +0100

    responsive page lawer

--- wp-content/plugins/tilleuls/src/profile-block/edit.js
+++ wp-content/plugins/tilleuls/src/profile-block/edit.js
@@ -1,11 +1,11 @@
 import {__} from '@wordpress/i18n';
 import {
-	useBlockProps,
-	RichText,
+	InnerBlocks,
 	InspectorControls,
 	MediaUpload,
 	MediaUploadCheck,
-	InnerBlocks
+	RichText,
+	useBlockProps
 } from '@wordpress/block-editor';
 import {Button, PanelBody, Tooltip} from '@wordpress/components';
 
@@ -17,8 +17,6 @@ export default function Edit({attributes, setAttributes}) {
 		languages,
 		diplomas
 	} = attributes;
-
-	console.log('diplomas ==>', diplomas)
 
 	const INNER_BLOCKS = [
 		['tilleuls-ortego-blocks/title-section', {}],
